ec6f418ae424171ea09abe15cd75638b07a4e2de
Ritesh Gadre <61073916+RiteshGadre@users.noreply.github.com>
2022-10-18 10:33:10 +0530

    Add files via upload

---
+++ Array/Medium/4. Kadane’s_Algorithm_maximum_subarray_sum.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+
+#define all(arr) arr.begin(), arr.end()
+using namespace std;
+#define insert_setToVec(st, vec) vec.insert(end(vec), begin(st), end(st))
+#define insert_vecToSet(vec, st) st.insert(begin(vec), end(vec))
+template<typename T>
+ostream& operator<<(ostream& os, vector<T>& intermediate_array) { for (auto& a : intermediate_array) cout << a << ' '; return os; }
+template<typename T>
+istream& operator>>(istream& is, vector<T>& intermediate_array) { for (auto& a : intermediate_array) cin >> a; return is; }
+#define deb(x) cerr << #x << " " << x << endl
+
+void solve(){
+	int n;
+	cin >> n;
+	vector<int> arr(n);
+	cin >> arr;
+	int sum= 0;
+	int mx= 0;
+	for(auto it: arr){
+		sum+= it;
+		sum= max(sum, it);
+		mx= max(mx, sum);
+	}
+	cout << mx << endl;
+}
+
+int main(){
+	#ifndef ONLINE_JUDGE
+        freopen("input.txt", "r" , stdin);
+        freopen("output.txt", "w" , stdout);
+    #endif
+    int tt=1;
+    cin >> tt; 
+    for(int i=0; i<tt; i++){
+    	solve();
+    }
+}
