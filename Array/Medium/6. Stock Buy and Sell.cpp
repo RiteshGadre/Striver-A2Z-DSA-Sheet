378c7ea836221eef37548ce6b567d70cff9e7e54
Ritesh Gadre <61073916+RiteshGadre@users.noreply.github.com>
2022-10-18 17:21:58 +0530

    Create 6. Stock Buy and Sell.cpp
    Leetcode Solution

---
+++ Array/Medium/6. Stock Buy and Sell.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+
+#define all(arr) arr.begin(), arr.end()
+using namespace std;
+#define insert_setToVec(st, vec) vec.insert(end(vec), begin(st), end(st))
+#define insert_vecToSet(vec, st) st.insert(begin(vec), end(vec))
+template<typename T>
+ostream& operator<<(ostream& os, vector<T>& intermediate_array) { for (auto& a : intermediate_array) cout << a << ' '; return os; }
+template<typename T>
+istream& operator>>(istream& is, vector<T>& intermediate_array) { for (auto& a : intermediate_array) cin >> a; return is; }
+#define deb(x) cerr << #x << " " << x << endl
+
+void solve(){
+	int n;
+	cin >> n;
+	vector<int> arr(n);
+	cin >> arr;
+	
+	int start= 0;
+	int mx= INT_MIN;
+	for(int i=1; i<n; i++){
+		if(arr[i]<arr[start]){
+			start= i;
+		}
+		mx= max(mx, arr[i]-arr[start]);
+		// cout << start << " " << arr[i] << " " << mx << endl ;
+	}
+	// cout << endl;
+	cout << mx << endl;
+
+
+}
+
+int main(){
+	#ifndef ONLINE_JUDGE
+        freopen("input.txt", "r" , stdin);
+        freopen("output.txt", "w" , stdout);
+    #endif
+    int tt=1;
+    cin >> tt; 
+    for(int i=0; i<tt; i++){
+    	solve();
+    }
+}
